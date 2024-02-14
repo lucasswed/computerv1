8bdbc933ad002d9a36e95ccecb8acb4a728c3cb9
wslHome <lucas@Lucas-Desktop>
2024-02-14 00:13:53 +0000

    Add the instructions on main to guide me

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+// Write a program that solves a polynomial second or lower degree equation. You will have
+// to show at least:
+// • The equation in its reduced form.
+// • The degree of the equation.
+// • It’s solution(s) and the polarity of the discriminant if it makes sens.
+
 int main(void) {
-  
+
 }
